33ae64fb740f3eed3bb5ef845e2b6cb01e8f5eea
Suzanne <STian@lcs.on.ca>
2022-02-22 09:05:07 -0500

    Initial Commit

---
+++ DadJokes/DadJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokesApp.swift
+//  DadJokes
+//
+//  Created by Suzanne Tian on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct DadJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
